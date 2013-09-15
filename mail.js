6082baf7ff1914a9c669fd0fd653c889198915f2
Mert Dümenci <mert@dumenci.me>
2013-09-15 14:55:20 +0300

    Add HTML email

--- mail.js
+++ mail.js
@@ -5,18 +5,41 @@
  */
 
 var config = require('./config'),
+    subscription = require('./subscription'),
     postmark = require('postmark')(config.defaults.postmarkAPIKey),
-    _ = require('underscore');
+    _ = require('underscore'),
+    async = require('async');
 
 exports.sendBatchNotification = function(emails, callback) {
-    var batch = _.map(emails, function(email) {
-        return {
-            From: config.defaults.email.from,
-            To: email,
-            Subject: config.defaults.email.subject,
-            TextBody: 'Joker\'iniz hazır!'
+    /*
+        Getting the subscription secrets is unfortunately async.
+    */
+
+    var queue = [];
+
+    _.each(emails, function(email) {
+        var operation = function(callback) {
+            generateHTMLForEmail(email, function(HTML) {
+                var postmarkEmail= {From: config.defaults.email.from,
+                                    To: email,
+                                    Subject: config.defaults.email.subject,
+                                    HtmlBody: HTML};
+
+                callback(null, postmarkEmail);
+            });
         };
+
+        queue.push(operation);
     });
 
-    postmark.batch(batch, callback);
+    async.series(queue, function(err, batch) {
+        postmark.batch(batch, callback);
+    });
 };
+
+function generateHTMLForEmail(email, callback) {
+    subscription.getSubscriptionSecret(email, function(secret) {
+        var HTML = "<div style='text-align: center;'><h1>Joker'iniz hazır!</h1><h2><a href='http://yemeksepeti.com'>Yemeksepeti.com</a>'dan Joker kampanyanız var</h2><a href='http://joker.dumenci.me/api/unwatch?email=" + email + "&secret=" + secret + "'>Bu maili bir daha almak istemiyorsanız tıklayınız</a></div>";
+        callback(HTML);
+    });
+}
